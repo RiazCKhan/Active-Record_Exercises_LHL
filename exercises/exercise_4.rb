0eeb176c797471dcedd9a74a8b229293dfc6a01a
RiazK <RiazCKhan@gmail.com>
2022-01-09 12:32:44 -0500

    Update instance var, removed 'stores' model duplication

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -16,17 +16,17 @@ puts "----------"
 
 # Your code goes here ...
 
-surrey = Stores.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
 surrey.save
 
-whistler = Stores.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 whistler.save
 
-yaletown = Stores.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 yaletown.save
 
-@mens_stores = Stores.where('mens_apparel = ?', true)
+@mens_stores = Store.where('mens_apparel = ?', true)
 # @mens_stores.each { | store | puts store.name}
 
-@womens_stores = Stores.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
 # @womens_stores.each { | store | puts store.name}
