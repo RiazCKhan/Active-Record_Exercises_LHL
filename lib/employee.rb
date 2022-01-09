e17c9925bb7cf5c26d29d9b9e66a36ffd9cbb57f
RiazK <RiazCKhan@gmail.com>
2022-01-09 16:01:38 -0500

    Update validation statements after mentor support

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,14 +1,15 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
-  validates_presence_of :first_name, presence: true
-  validates_numericality_of :hourly_rate, { only_integer: true, minimum: 40, maximum: 200}
-  validates_presence_of :store_id, presence: true
+  #   * Employees must always have a first name present
+  validates :first_name, presence: true
+  #   * Employees must always have a last name present
+  validates :last_name, presence: true
+  #   * Employees have a hourly_rate that is a number (integer) between 40 and 200
+  validates :hourly_rate, numericality: { minimum: 40, maximum: 200}
+  #   * Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+  validates :store_id, presence: true
 end
 
-#   * Employees must always have a first name present
-#   * Employees must always have a last name present
-#   * Employees have a hourly_rate that is a number (integer) between 40 and 200
-#   * Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
 
 
