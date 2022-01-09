0c1dc8c2190d5142bdf7354217e21bdc47e19aa9
RiazK <RiazCKhan@gmail.com>
2022-01-09 13:42:50 -0500

    Add validations for both tables

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates_length_of_name :name { minimum: 3 }
+  validates_numericality_of :annual_revenue { only_integer: true }
 end
+
+#   * Stores must always have a name that is a minimum of 3 characters
+#   * Stores have an annual_revenue that is a number (integer) that must be 0 or more
